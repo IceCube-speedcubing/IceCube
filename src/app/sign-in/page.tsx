df13a0ac32fc387df8e50f3cf1f5f17fa26e3742
myjak <patrick@myjak.dk>
2024-03-28 22:54:30 +0100

    updateed sign-in and sign-up pages

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,69 +1,63 @@
-import Image from "next/image"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-const signInPage = () => {
+const SignInPage = () => {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-[#001A2C]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-gray-50">Login</h1>
-            <p className="text-balance text-gray-300">
-              Enter your email below to login to your account
-            </p>
+    <div className="bg-[#001A2C] pt-5 pb-10 h-screen flex items-center justify-center">
+        <Card className="mx-auto max-w-sm min-w-[400px]  bg-[#003356] border-0">
+      <CardHeader>
+        <CardTitle className="text-2xl text-[#fff]">Login</CardTitle>
+        <CardDescription className="text-gray-300">
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email" className="text-[#fff]">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
           </div>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email" className="text-gray-200">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password" className="text-[#fff]">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline text-gray-300">
+                Forgot your password?
+              </Link>
             </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password" className="text-gray-200">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline text-gray-300"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full bg-[#003A61]">
-              Login
-            </Button>
-            <Button variant="outline" className="w-full">
-              Login with WCA
-            </Button>
+            <Input id="password" type="password" required />
           </div>
-          <div className="mt-4 text-center text-sm text-gray-200">
-            Don&apos;t have an account?{" "}
-            <Link href="/sign-up" className="underline">
-              Sign up
-            </Link>
-          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with WCA
+          </Button>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
+        <div className="mt-4 text-center text-sm text-gray-200">
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
     </div>
   )
 }
 
-export default signInPage
+export default SignInPage
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,72 +1,67 @@
-import Image from "next/image"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-const signUpPage = () => {
+const SignUpPage = () => {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-[#001A2C]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold text-gray-50">Sign Up</h1>
-            <p className="text-balance text-gray-300">
-              Enter your email below to create an account
-            </p>
-          </div>
-          <div className="grid gap-4">
+    <div className="bg-[#001A2C] pt-5 pb-10 h-screen flex items-center justify-center">
+        <Card className="mx-auto max-w-sm min-w-[400px] bg-[#003356] border-0">
+      <CardHeader>
+        <CardTitle className="text-2xl text-[#fff]">Sign up</CardTitle>
+        <CardDescription className="text-gray-300">
+          Create an account below to get started.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
           <div className="grid gap-2">
-              <Label htmlFor="text" className="text-gray-200">Username</Label>
-              <Input
-                id="username"
-                type="text"
-                placeholder="username"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email" className="text-gray-200">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password" className="text-gray-200">Password</Label>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-            <Button type="submit" className="w-full bg-[#003A61]">
-              Sign up
-            </Button>
-            <Button variant="outline" className="w-full">
-              Sign with WCA
-            </Button>
+          <Label htmlFor="text" className="text-[#fff]">Username</Label>
+            <Input
+              id="username"
+              type="text"
+              placeholder="username"
+              required
+            />
+            <Label htmlFor="email" className="text-[#fff]">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
           </div>
-          <div className="mt-4 text-center text-sm text-gray-200">
-            Already have an account?{" "}
-            <Link href="/sign-in" className="underline">
-              Sign in
-            </Link>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password" className="text-[#fff]">Password</Label>
+            </div>
+            <Input id="password" type="password" required />
           </div>
+          <Button type="submit" className="w-full">
+            Sign up
+          </Button>
+          <Button variant="outline" className="w-full">
+            Sing up with WCA
+          </Button>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
-      </div>
+        <div className="mt-4 text-center text-sm text-gray-200">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
     </div>
   )
 }
 
-export default signUpPage
+export default SignUpPage
