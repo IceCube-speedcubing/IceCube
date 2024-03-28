59dfa3d5a95c425f7c33d2935e32917cec59322c
myjak <patrick@myjak.dk>
2024-03-28 16:49:59 +0100

    start on alg card(img not working)

--- src/app/algs/page.tsx
+++ src/app/algs/page.tsx
@@ -6,25 +6,32 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import Image from "next/image";
 
 const AlgsPage = () => {
-
-    
-
   return (
     <>
-      <div className="bg-[#001A2C]">
-      <div >
-        <div className="py-10 mx-auto text-center flex flex-col items-center max-w-3xl">
-          <h1 className="text-1xl font-bold tracking-tight text-gray-100  sm:text-3xl">
-            All the best algorithms for all events.
-          </h1>
+      <div className="bg-[#001A2C] pb-10">
+        <div>
+          <div className="py-10 mx-auto text-center flex flex-col items-center max-w-3xl">
+            <h1 className="text-1xl font-bold tracking-tight text-gray-100  sm:text-3xl">
+              All the best algorithms for all events.
+            </h1>
+          </div>
         </div>
+        <Card className="w-[350px] bg-[#002945] border-[#00406C] border-4 m-5">
+          <CardHeader className="bg-[003A61]">
+            <CardTitle className="text-gray-100 font-bold">Ua</CardTitle>
+            <CardDescription>
+              <img src="/assets/PLL/Ua.png"></img>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-gray-300">( R U R' U R' ) U' R2 U' ( R' U R' U R )</p>
+          </CardContent>
+        </Card>
       </div>
-
-      </div>
-      </>
+    </>
   );
 };
 
