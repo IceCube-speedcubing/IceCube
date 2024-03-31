b87ffadee1f9fd9ca1b5505685e3abb0c5cbb420
JamesCraft10 <69867626+jamescraft10@users.noreply.github.com>
2024-03-30 20:34:05 -0400

    backend

---
+++ src/backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const fs = require('fs'); 
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URL);
+const db = mongoose.connection;
+db.on('error', (e) => console.error(e));
+db.on('open', () => console.log('Connected to Database'));
+
+const apiRouter = require('./api.js');
+
+app.use(express.static("out"));
+app.use(express.json());
+app.use('/api', apiRouter);
+
+// 404
+app.use(function (req, res, next) {
+    fs.readFile('./out/404.html', function(err, data) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+})
+
+app.listen(8080, () => {
+    console.log('listening on *:8080');
+});
