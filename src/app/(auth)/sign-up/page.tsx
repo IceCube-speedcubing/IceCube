7fbe86d0120d19d08785e2b5ec9c54079ec3cef5
myjak <patrick@myjak.dk>
2024-03-31 01:19:23 +0100

    frondend

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const SignUpPage = () => {
+  return (
+    <>
+    <div className="h-screen flex items-center justify-center">
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" placeholder="username" required />
+            </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+          <Button type="submit" className="w-full">
+            Create an account
+          </Button>
+          <Button variant="outline" className="w-full">
+            Sign up with WCA
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+    </div>
+    </> 
+  )
+}
+export default SignUpPage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,8 @@ import UserAccountNav from "./UserAccountNav";
 
 
 const Navbar = async () => {
+
+  const user = true
   
   return (
     <div className="bg-white  sticky z-50 top-0 inset-0 inset-x-0 h-16">
@@ -29,8 +31,34 @@ const Navbar = async () => {
 
               <div className='ml-auto flex items-center'>
                 <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
-                  
-                  
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <UserAccountNav />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
                 </div>
               </div>
             </div>
