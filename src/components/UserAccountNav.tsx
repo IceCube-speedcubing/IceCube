4e321da4a5138e829ef9c1b64f245327b5de95a1
myjak <patrick@myjak.dk>
2024-05-13 11:06:11 +0200

    fixed some dark mode

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -24,16 +24,16 @@ const UserAccountNav = () => {
                 </Button>
             </DropdownMenuTrigger>
 
-            <DropdownMenuContent className="bg-white w-60" align="end">
+            <DropdownMenuContent className="bg-white w-60 dark:bg-[#002d4a]" align="end">
                 <div className="flex items-center justify-start gap-2 p-2">
                     <div className="flex flex-col space-y-0.5 leading-none">
-                        <p className="font-medium text-dm text-black">
+                        <p className="font-bold text-dm">
                             Username
                         </p>
                     </div>
                 </div>
 
-                <DropdownMenuSeparator className="h-0.5 bg-gray-300" />
+                <DropdownMenuSeparator className="h-0.5 bg-gray-300 dark:bg-[#00406C]" />
 
                 <DropdownMenuItem asChild>
                     <Link href="/settings/general">Settings</Link>
