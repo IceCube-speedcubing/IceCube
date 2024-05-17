99f0e1cf6817b8047c9956d6ec12bd0207a1b017
myjak <patrick@myjak.dk>
2024-05-17 18:14:27 +0200

    tryed to conect sign-up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,27 +1,36 @@
-'use client';
+'use client'
 
-import Link from "next/link";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { EyeOff, Eye } from "lucide-react";
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 const SignUpPage = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  })
+  const [error, setError] = useState('')
+  const [showAlert, setShowAlert] = useState(false)
+  const router = useRouter()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
       const response = await fetch('/api/user', {
@@ -29,95 +38,123 @@ const SignUpPage = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, email, password }),
-      });
-
-      const data = await response.json();
+        body: JSON.stringify(formData),
+      })
 
       if (response.ok) {
-        console.log(data.message); // Handle successful sign-up
+        // Handle successful sign-up
+        console.log('Sign-up successful')
+        router.push('/sign-in') // Redirect to sign-in page
+      } else if (response.status === 404) {
+        // Handle 404 Not Found error
+        setError('The sign-up endpoint is not available. Please contact the website administrators for assistance.')
+        setShowAlert(true)
       } else {
-        console.error(data.message); // Handle sign-up error
+        // Handle other errors
+        const responseText = await response.text()
+        let errorMessage
+
+        try {
+          const errorData = JSON.parse(responseText)
+          errorMessage = errorData.message
+        } catch (error) {
+          // If the response is not valid JSON, use the response text as the error message
+          errorMessage = responseText
+        }
+
+        console.error('Sign-up error:', errorMessage)
+        setError(errorMessage)
+        setShowAlert(true)
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error('An error occurred:', error)
+      setError('An error occurred. Please try again later.')
+      setShowAlert(true)
     }
-  };
+  }
 
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+  useEffect(() => {
+    let alertTimeout;
+
+    if (showAlert) {
+      alertTimeout = setTimeout(() => {
+        setShowAlert(false);
+      }, 5000); // Hide the alert after 5 seconds
+    }
+
+    return () => {
+      clearTimeout(alertTimeout);
+    };
+  }, [showAlert]);
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Card className="mx-auto max-w-sm">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      {/* Error Alert Popup */}
+      {showAlert && (
+        <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 -mt-20">
+          <Alert variant="destructive" className="max-w-xs">
+            <AlertTitle className="text-sm">Error</AlertTitle>
+            <AlertDescription className="text-sm">{error}</AlertDescription>
+          </Alert>
+        </div>
+      )}
+
+      <Card className="max-w-md w-full p-8">
         <CardHeader>
-          <CardTitle className="text-xl">Sign Up</CardTitle>
-          <CardDescription>
-            Enter your information to create an account
-          </CardDescription>
+          <CardTitle className="text-2xl font-bold">Sign Up</CardTitle>
+          <CardDescription>Enter your information to create an account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  placeholder="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <div className="relative">
-                  <Input
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    className="absolute right-2 top-1"
-                    onClick={toggleShowPassword}
-                  >
-                    {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
-                  </Button>
-                </div>
-              </div>
-              <Button type="submit" className="w-full">
-                Create an account
-              </Button>
-              <Button variant="outline" className="w-full">
-                Sign up with WCA
-              </Button>
+          <form onSubmit={handleSubmit} className="grid gap-4">
+            <div>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
             </div>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
           </form>
           <div className="mt-4 text-center text-sm">
-            Already have an account?{" "}
-            <Link href="/sign-in" className="underline">
+            Already have an account?{' '}
+            <Link href="/sign-in" className="text-blue-500 underline">
               Sign in
             </Link>
           </div>
         </CardContent>
       </Card>
     </div>
-  );
-};
+  )
+}
 
-export default SignUpPage;
+export default SignUpPage
