b1be8b464eef32f5474c4414ea905d9a8a10dcab
JamesCraft10 <69867626+jamescraft10@users.noreply.github.com>
2024-07-21 19:51:35 -0400

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,14 +8,6 @@ const db = mongoose.connection;
 db.on('error', (e) => console.error(e));
 db.on('open', () => console.log('Connected to Database'));
 
-// dont use in production
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    next();
-});
-
 const apiRouter = require('./api.js');
 
 app.use(express.static("out"));
